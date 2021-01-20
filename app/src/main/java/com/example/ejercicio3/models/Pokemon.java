ea0fafdc00fe3e21369d29d24b932909cdad9eb9
seraf7 <zaske_4@hotmail.com>
2021-01-20 10:56:56 -0600

    Elemento generico del menú

---
+++ app/src/main/java/com/example/ejercicio3/models/Pokemon.java
@@ -0,0 +1,82 @@
+package com.example.ejercicio3.models;
+
+public class Pokemon {
+    private long id;
+    private String nombre;
+    private String url;
+    private  String urlImg;
+    private int expBase;
+    private int altura;
+    private int peso;
+    private String[] tipo;
+
+    public Pokemon(long id, String nombre, String url) {
+        this.id = id;
+        this.nombre = nombre;
+        this.url = url;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public int getExpBase() {
+        return expBase;
+    }
+
+    public void setExpBase(int expBase) {
+        this.expBase = expBase;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String[] getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String[] tipo) {
+        this.tipo = tipo;
+    }
+}
