d86e361c9a515a4763a344c62c8fa5a5c15ccc4d
seraf7 <zaske_4@hotmail.com>
2021-01-20 13:18:21 -0600

    Implementción de RecyclerView y correcciones de aspecto

--- app/src/main/java/com/example/ejercicio3/MainActivity.java
+++ app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.ejercicio3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.ejercicio3.models.Pokemon;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvListado;
+    GridLayoutManager glm;
+    PokemonAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asociación de elementos de la vista
+        rvListado = findViewById(R.id.rvListado);
+
+        //Instancia de GridLayourManager con 2 columnas
+        glm = new GridLayoutManager(this, 2);
+        //Uso de RecyclerView con dos columnas
+        rvListado.setLayoutManager(glm);
+        //Configuracion del Adapter
+        adapter = new PokemonAdapter(llenado());
+        rvListado.setAdapter(adapter);
+    }
+
+    private ArrayList<Pokemon> llenado(){
+        ArrayList<Pokemon> datos = new ArrayList<>();
+        for(int i = 0; i<30; i++){
+            Pokemon pokemon = new Pokemon(i, getResources().getString(R.string.pokemon)+i, "URL");
+            datos.add(pokemon);
+        }
+        return datos;
     }
 }
---
+++ app/src/main/java/com/example/ejercicio3/PokemonAdapter.java
@@ -0,0 +1,59 @@
+package com.example.ejercicio3;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.ejercicio3.models.Pokemon;
+import java.util.ArrayList;
+
+public class PokemonAdapter extends RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>{
+
+    private ArrayList<Pokemon> datos;
+
+    public PokemonAdapter(ArrayList<Pokemon> datos){
+        this.datos = datos;
+    }
+
+    //Crea una vista PokemonViewHolder y la asocia a los elementos de item_pokemon
+    @Override
+    public PokemonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Instancia de vista item_pokemon
+        return new PokemonViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pokemon, parent, false));
+    }
+
+    //Inyecta la informacion de los elementos de la colección a cada vista
+    @Override
+    public void onBindViewHolder(PokemonViewHolder holder, int position) {
+        //Obtiene elemento del arreglo
+        Pokemon tmpPokemon = datos.get(position);
+        //Establece contenido de etiquetas
+        holder.tvNombre.setText(tmpPokemon.getNombre());
+        holder.tvNumero.setText(String.valueOf(tmpPokemon.getId()));
+    }
+
+    //Devuelve la cantidad de elementos de la colección
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+    //Clase para representar cada vista de la coleccion
+    class PokemonViewHolder extends RecyclerView.ViewHolder{
+        //Atributos que contiene cada elemento
+        ImageView ivCard;
+        TextView tvNumero;
+        TextView tvNombre;
+
+        //Constructor de la clase
+        public PokemonViewHolder(View itemView) {
+            super(itemView);
+            //Asociacion de elementos de la vista
+            ivCard = itemView.findViewById(R.id.ivCard);
+            tvNumero = itemView.findViewById(R.id.tvNumero);
+            tvNombre = itemView.findViewById(R.id.tvNombre);
+        }
+    }
+}
